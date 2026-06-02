d1429fdd16aeaed948d12178fa7bcafa14787fbf
unknown <Mike2024New>
2026-06-03 01:07:04 +0300

    commit ac3bcf

--- src/infrastructure/src/infrastructure/builder/main.py
+++ src/infrastructure/src/infrastructure/builder/main.py
@@ -3,6 +3,9 @@ import subprocess
 import platform
 from pathlib import Path
 from infrastructure.path_utils.path_finder import get_root_dir_path
+from infrastructure.path_utils.symlinks import create_symlink
+import shutil
+import os
 
 """
 Сборщик исполнительных файлов .exe для windows, и .bin для linux
@@ -19,7 +22,9 @@ def build(
         console: bool = True,
         icon_path: Path = Path('icon.ico'),
         venv_dir_name: str = '.venv',
-)->None:
+        create_resources_symlink: bool = False,
+        clear_old_distributive: bool = True,
+) -> None:
     """
     Сборщик исполнительных файлов .exe для windows, и .bin для linux.
     Путь к .venv определяется автоматически
@@ -27,22 +32,25 @@ def build(
     :param name: имя выходного приложения
     :param add_data:  устанавливаемые дополнительные ассеты
     :param add_binary: устанавливаемые .dll
-    :param exclude_modules: исключаемые модули и файлы
-    :param one_file:  собрать .exe одним файлом?
+    :param exclude_modules: исключаемые модули и файлы (поможет облегчить сборку)
+    :param one_file:  собрать .exe одним файлом? (дольше по времени загрузка приложения)
     :param console: приложение консольное?
+    :param create_resources_symlink: создавать симлинк на папку с ресурсами?
     :param icon_path: путь к иконке
     :param venv_dir_name: название папки с виртуальным окружением (как правило обычно всегда .venv)
+    :param clear_old_distributive: удалить следы предыдущих дистрибутивов?
     :return: None
     Пример использования:
 
     build(
-        entry_point_path=Path(r'S:\_projects\python\SERVICES\STT\main.py'),
-        name='STT',
-        add_data=[Path('faster_whisper') / 'assets'],
-        add_binary=[],
-        exclude_modules=[],
-        icon_path=Path('icon.ico'),
-        one_file=False,
+        entry_point_path=get_root_dir_path() / 'main.py', # путь к файлу с которого начинается приложение
+        name='STT', # имя приложения будет отображаться например STT.exe
+        add_data=[Path('faster_whisper') / 'assets'], # дополнительные материалы
+        add_binary=[], # .dll и прочие библиотеки
+        exclude_modules=[], # исключаемые библиотеки (поможет облегчить сборку)
+        icon_path=Path('icon.ico'), # путь к иконке (как правило лежит в root_dir)
+        one_file=True, # сжать всё в один файл? (дольше по времени загрузка приложения)
+        create_resources_symlink=True, # создать симлинк на папку с ресурсами? Если она есть в проекте.
     )
     """
     print('[green]Сборка приложения[/green]')
@@ -54,11 +62,12 @@ def build(
         RuntimeError(f'Входной путь `{entry_point_path}` не существует.')
 
     # определение системных путей
+    root_dir = get_root_dir_path(venv_dir_name=venv_dir_name)
     is_windows = platform.system().lower() == 'windows'
     separator = ";" if is_windows else ":"
     ext = ".dll" if is_windows else ".so"
     python_lib = f"Lib\\site-packages" if is_windows else f"lib/python{sys.version_info.major}.{sys.version_info.minor}/site-packages"
-    python_lib = get_root_dir_path(venv_dir_name=venv_dir_name) / '.venv' / python_lib
+    python_lib = root_dir / '.venv' / python_lib
 
     cmd = [
         sys.executable, '-m', 'PyInstaller',
@@ -98,9 +107,20 @@ def build(
         cmd.append(f'--icon={str(icon_path)}')
 
     cmd.append(str(entry_point_path))
-    print(cmd)
 
-    distributive_path = Path(__file__).parent / 'dist'
+    distributive_path = root_dir / 'dist'
+    build_folder_path = root_dir / 'build'
+    file_spec_path = root_dir / f'{name}.spec'
+
+    # очистка следов предыдущего проекта
+    if clear_old_distributive:
+        if distributive_path.exists():
+            shutil.rmtree(distributive_path)
+        if build_folder_path.exists():
+            shutil.rmtree(build_folder_path)
+        if file_spec_path.exists():
+            os.remove(file_spec_path)
+
     result = subprocess.run(cmd, shell=is_windows)
     if result.returncode != 0:
         raise RuntimeError(
@@ -108,5 +128,17 @@ def build(
             f'returncode: {result.returncode}\n'
             f'stdout: {result.stdout}\n'
             f'stderr: {result.stderr}\n'
+        )
+
+    resources_dir = root_dir / 'resources'
+    if create_resources_symlink:
+        if not resources_dir.exists():
+            print(f'Симлинк не создан так как в корне отсутствует папка resources.')
+        # создание симлинка на папку с ресурсами
+        create_symlink(
+            source_directory=root_dir / 'resources',
+            # если не один файл то симлинк вложенный
+            target_directory=distributive_path / name if not one_file else distributive_path,
+
         )
     print(f'[green]Приложение собрано. {distributive_path.parent}[/green]')
