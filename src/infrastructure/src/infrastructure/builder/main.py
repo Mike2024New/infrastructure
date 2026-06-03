05f49826d4422e02f6ce186f44da6d12e941622b
unknown <Mike2024New>
2026-06-03 17:23:06 +0300

    commit e3c0cc

--- src/infrastructure/src/infrastructure/builder/main.py
+++ src/infrastructure/src/infrastructure/builder/main.py
@@ -6,33 +6,23 @@ from infrastructure.path_utils.path_finder import get_root_dir_path
 from infrastructure.path_utils.symlinks import create_symlink
 import shutil
 import os
+from dataclasses import dataclass, field
 
 """
 Сборщик исполнительных файлов .exe для windows, и .bin для linux
 """
 
+ROOT_DIR = get_root_dir_path()
 
-def build(
-        entry_point_path: Path,
-        name: str,
-        add_data: list[Path] | None = None,
-        add_binary: list[Path] | None = None,
-        exclude_modules: list[Path] | None = None,
-        one_file: bool = False,
-        console: bool = True,
-        icon_path: Path = Path('icon.ico'),
-        venv_dir_name: str = '.venv',
-        create_resources_symlink: bool = False,
-        clear_old_distributive: bool = True,
-) -> None:
+
+@dataclass
+class BuildParameters:
     """
-    Сборщик исполнительных файлов .exe для windows, и .bin для linux.
-    Путь к .venv определяется автоматически
     :param entry_point_path:
     :param name: имя выходного приложения
     :param add_data:  устанавливаемые дополнительные ассеты
     :param add_binary: устанавливаемые .dll
-    :param exclude_modules: исключаемые модули и файлы (поможет облегчить сборку)
+    :param excluded: исключаемые модули и файлы (поможет облегчить сборку)
     :param one_file:  собрать .exe одним файлом? (дольше по времени загрузка приложения)
     :param console: приложение консольное?
     :param create_resources_symlink: создавать симлинк на папку с ресурсами?
@@ -40,29 +30,54 @@ def build(
     :param venv_dir_name: название папки с виртуальным окружением (как правило обычно всегда .venv)
     :param clear_old_distributive: удалить следы предыдущих дистрибутивов?
     :return: None
-    Пример использования:
-
-    build(
+    Пример заполнения:
+    BuildParameters(
         entry_point_path=get_root_dir_path() / 'main.py', # путь к файлу с которого начинается приложение
         name='STT', # имя приложения будет отображаться например STT.exe
         add_data=[Path('faster_whisper') / 'assets'], # дополнительные материалы
         add_binary=[], # .dll и прочие библиотеки
-        exclude_modules=[], # исключаемые библиотеки (поможет облегчить сборку)
+        excluded=[], # исключаемые библиотеки (поможет облегчить сборку)
         icon_path=Path('icon.ico'), # путь к иконке (как правило лежит в root_dir)
         one_file=True, # сжать всё в один файл? (дольше по времени загрузка приложения)
         create_resources_symlink=True, # создать симлинк на папку с ресурсами? Если она есть в проекте.
     )
     """
+    name: str = 'APP'
+    entry_point_path: Path = ROOT_DIR / 'main.py'
+    icon_path: Path = ROOT_DIR / 'resources' / 'images' / 'icon.ico'
+    one_file: bool = True
+    console: bool = True
+    create_resources_symlink: bool = True
+    clear_old_distributive: bool = True
+    # не трогать, поля ниже. Заполнятся автоматически (то что в них попадет определяется в app/_engine/registry.py
+    add_data: list[Path] = field(default_factory=list)
+    add_binary: list[Path] = field(default_factory=list)
+    excluded: list[Path] = field(default_factory=list)
+    venv_dir_name: str = '.venv'
+
+
+def build(parameters: BuildParameters) -> None:
+    """
+    Сборщик исполнительных файлов .exe для windows, и .bin для linux.
+    Путь к .venv определяется автоматически
+    :param parameters:  Класс BuildParameters с настройками
+    :return: None
+    Пример использования:
+
+    build(
+        parameters = BuildParameters( ... настройки приложения ... )
+    )
+    """
     print('[green]Сборка приложения[/green]')
 
     # проверка входного пути
-    if entry_point_path.parts[-1] != '.py':
-        RuntimeError(f'Входной путь должен быть .py файлом, а на вход подан `{entry_point_path}`')
-    if not entry_point_path.exists():
-        RuntimeError(f'Входной путь `{entry_point_path}` не существует.')
+    if parameters.entry_point_path.parts[-1] != '.py':
+        RuntimeError(f'Входной путь должен быть .py файлом, а на вход подан `{parameters.entry_point_path}`')
+    if not parameters.entry_point_path.exists():
+        RuntimeError(f'Входной путь `{parameters.entry_point_path}` не существует.')
 
     # определение системных путей
-    root_dir = get_root_dir_path(venv_dir_name=venv_dir_name)
+    root_dir = get_root_dir_path(venv_dir_name=parameters.venv_dir_name)
     is_windows = platform.system().lower() == 'windows'
     separator = ";" if is_windows else ":"
     ext = ".dll" if is_windows else ".so"
@@ -73,11 +88,11 @@ def build(
     releases_folder_path = root_dir / 'releases' / 'win' if is_windows else root_dir / 'releases' / 'linux'
     distributive_path = releases_folder_path / 'dist'
     build_folder_path = releases_folder_path / 'build'
-    file_spec_path = releases_folder_path / f'{name}.spec'
+    file_spec_path = releases_folder_path / f'{parameters.name}.spec'
 
     cmd = [
         sys.executable, '-m', 'PyInstaller',
-        '--name', name,
+        '--name', parameters.name,
         f'--distpath', distributive_path,
         '--exclude-module', Path(__file__).stem,
         '--workpath', build_folder_path,
@@ -85,13 +100,13 @@ def build(
     ]
 
     # исключаемые модули
-    if exclude_modules is not None:
-        for e in exclude_modules:
+    if parameters.excluded is not None:
+        for e in parameters.excluded:
             cmd.extend(['--exclude-module', e])
 
     # сборка бинарных файлов (.dll, библиотек)
-    if add_binary is not None:
-        for binary in add_binary:
+    if parameters.add_binary is not None:
+        for binary in parameters.add_binary:
             binary_path = python_lib / binary
             # сборка .dll или .so файлов
             for f in binary_path.iterdir():
@@ -99,26 +114,26 @@ def build(
                     cmd.extend(['--add-binary', f'{f}{separator}{binary}'])
 
     # сборка data
-    if add_data is not None:
-        for d in add_data:
+    if parameters.add_data is not None:
+        for d in parameters.add_data:
             data_path = python_lib / d
             cmd.extend(['--add-data', f'{str(data_path)}{separator}{d}'])
 
     # собирать приложение 1 файлом
-    if one_file:
+    if parameters.one_file:
         cmd.append('--onefile')
 
     # приложение консольное или нет
-    cmd.append('--console') if console else cmd.append('--noconsole')
+    cmd.append('--console') if parameters.console else cmd.append('--noconsole')
 
     # добавление иконки
-    if icon_path.exists():
-        cmd.append(f'--icon={str(icon_path)}')
+    if parameters.icon_path.exists():
+        cmd.append(f'--icon={str(parameters.icon_path)}')
 
-    cmd.append(str(entry_point_path))
+    cmd.append(str(parameters.entry_point_path))
 
     # очистка следов предыдущего проекта
-    if clear_old_distributive:
+    if parameters.clear_old_distributive:
         if distributive_path.exists():
             shutil.rmtree(distributive_path)
         if build_folder_path.exists():
@@ -136,14 +151,14 @@ def build(
         )
 
     resources_dir = root_dir / 'resources'
-    if create_resources_symlink:
+    if parameters.create_resources_symlink:
         if not resources_dir.exists():
             print(f'Симлинк не создан так как в корне отсутствует папка resources.')
         # создание симлинка на папку с ресурсами
         create_symlink(
             source_directory=root_dir / 'resources',
             # если не один файл то симлинк вложенный
-            target_directory=distributive_path / name if not one_file else distributive_path,
+            target_directory=distributive_path / parameters.name if not parameters.one_file else distributive_path,
 
         )
     print(f'[green]Приложение собрано. {distributive_path.parent}[/green]')
