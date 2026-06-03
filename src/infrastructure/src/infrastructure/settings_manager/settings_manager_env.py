49e86565d4a32264269cf1a4f33a63651ce1cffc
unknown <Mike2024New>
2026-06-04 02:21:31 +0300

    commit 669054

--- src/infrastructure/src/infrastructure/settings_manager/settings_manager_env.py
+++ src/infrastructure/src/infrastructure/settings_manager/settings_manager_env.py
@@ -2,6 +2,7 @@ from infrastructure.path_utils.path_finder import get_root_dir_path
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Type, TypeVar
+from pathlib import Path
 
 _T = TypeVar('_T', bound=BaseSettings)
 
@@ -15,10 +16,11 @@ class SettingsEnvModel(BaseSettings):
 
 
 @lru_cache(maxsize=1)
-def load_settings_env(settings_class: Type[_T], env_prefix: str) -> _T:
+def load_settings_env(settings_class: Type[_T], env_path: Path, env_prefix: str) -> _T:
     """
     Загрузка pydantic-settings конфигурации из .env
     :param settings_class: Pydantic-settings класс с описанными полями
+    :param env_path: путь к файлу .env
     :param env_prefix: префикс полей, например POSTGRES_
     :return: настройки прочиннанные из settings_class
 
@@ -35,8 +37,13 @@ def load_settings_env(settings_class: Type[_T], env_prefix: str) -> _T:
     POSTGRES_USER_NAME='YourName'
     --------------------------------------------------------
     """
-    env_file = get_root_dir_path() / '.env'
-    return settings_class(_env_file=env_file, _env_prefix=env_prefix)
+    if not env_path.parts[-1].endswith('.env'):
+        raise RuntimeError(f'Путь env_path нужно указать именно к `.env` файлу, сейчас указан `{env_path}`')
+
+    if not env_path.exists():
+        raise RuntimeError(f'Файл env_path = `{env_path}` не найден')
+
+    return settings_class(_env_file=env_path, _env_prefix=env_prefix)
 
 
 if __name__ == '__main__':
@@ -44,5 +51,9 @@ if __name__ == '__main__':
         user_name: str = ...
 
 
-    env_settings = load_settings_env(settings_class=MySettings, env_prefix='POSTGRES_')
+    env_settings = load_settings_env(
+        settings_class=MySettings,
+        env_path=get_root_dir_path() / '.env',
+        env_prefix='POSTGRES_',
+    )
     print(env_settings.user_name)
