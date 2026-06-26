1620d9ab9716a70a0b4a5268df6916d0724c2b14
Mike2024New <proggerman2022@yandex.ru>
2026-06-26 18:27:46 +0300

    commit 52fd75

--- infrastructure/http_clients/__init__.py
+++ infrastructure/http_clients/__init__.py
@@ -1,3 +1,7 @@
 from infrastructure.http_clients.downloader import Downloader
+from infrastructure.http_clients.adapters.hf_adapter import adpater_download_from_hf
 
-__all__ = ['Downloader']
+__all__ = [
+    'Downloader',  # загрузчик
+    'adpater_download_from_hf'  # адаптеры для загрузки
+]
