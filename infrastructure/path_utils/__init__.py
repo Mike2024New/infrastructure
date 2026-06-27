d72ffcaa133eeb77fcdc9f92ae1c94b6cdd46e7d
Mike2024New <proggerman2022@yandex.ru>
2026-06-27 18:08:55 +0300

    commit c2eb2c

--- infrastructure/path_utils/__init__.py
+++ infrastructure/path_utils/__init__.py
@@ -2,10 +2,12 @@ from infrastructure.path_utils.path_finder import get_root_dir_path
 from infrastructure.path_utils.open_folder import open_folder
 from infrastructure.path_utils.rotate_file import rotate_file_by_size
 from infrastructure.path_utils.symlinks import create_symlink
+from infrastructure.path_utils.flat_json_manager import FlatJsonManager
 
 __all__ = [
     'get_root_dir_path',
     'open_folder',
     'rotate_file_by_size',
     'create_symlink',
+    'FlatJsonManager',  # плоский json менеджер для структур вида { key : val }
 ]
